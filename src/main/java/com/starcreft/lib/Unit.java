006702c1c2f756639ec3f2119b8c9123c73e07de
50000 <chg3376@gmail.com>
2021-12-15 23:19:59 +0900

    first commit

---
+++ src/main/java/com/starcreft/lib/Unit.java
@@ -0,0 +1,9 @@
+package com.starcreft.lib;
+
+public interface Unit {
+    public void moving();
+    public void stop();
+    public void attack();
+    public void reconnaissance();
+    public void positionShooter();
+}
---
+++ src/test/java/com/starcreft/starcreft/StarcreftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starcreft.starcreft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarcreftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
